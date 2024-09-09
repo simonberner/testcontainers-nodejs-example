f87adf581949d787a1c16545bcd793cd592c992a
Simon Berner <sibebzh@gmail.com>
2024-09-09 09:22:04 +0200

    test: :wrench: update playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,42 +1,48 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-    // Look for test files in the "tests" directory, relative to this configuration file.
     testDir: 'tests/web',
-
-    // Run all tests in parallel.
+    /* Run tests in files in parallel */
     fullyParallel: true,
-
-    // Fail the build on CI if you accidentally left test.only in the source code.
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
-
-    // Retry on CI only.
+    /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
-
-    // Opt out of parallel tests on CI.
+    /* Run tests in parallel on CI */
     workers: process.env.CI ? 1 : undefined,
-
-    // Reporter to use
-    reporter: 'html',
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: 'list',
 
     use: {
-        // Base URL to use in actions like `await page.goto('/')`.
-        baseURL: 'http://127.0.0.1:3000',
-
-        // Collect trace when retrying the failed test.
+        // Update baseURL to match your Testcontainers setup
+        baseURL: 'http://localhost:8080',
         trace: 'on-first-retry',
     },
-    // Configure projects for major browsers.
+
     projects: [
         {
             name: 'chromium',
             use: { ...devices['Desktop Chrome'] },
         },
+        // {
+        //     name: "firefox",
+        //     use: { ...devices["Desktop Firefox"] },
+        // },
+
+        // {
+        //     name: "webkit",
+        //     use: { ...devices["Desktop Safari"] },
+        // },
     ],
-    // Run your local dev server before starting the tests.
-    webServer: {
-        command: 'npm run start',
-        url: 'http://127.0.0.1:3000',
-        reuseExistingServer: !process.env.CI,
-    },
+
+    /* Run your local dev server before starting the tests */
+    // webServer: {
+    //   command: 'npm run start',
+    //   url: 'http://127.0.0.1:3000',
+    //   reuseExistingServer: !process.env.CI,
+    // },
+
+    // Add globalSetup and globalTeardown
+    //globalSetup: require.resolve('./global-setup'),
+    //globalTeardown: require.resolve('./global-teardown'),
 });
