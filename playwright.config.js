d1f90caa0ad2c7140bb4fadb8e34a814882c27cd
Simon Berner <sibebzh@gmail.com>
2025-02-27 17:52:39 +0100

    test: :wrench: add mobile view ports
    Add mobile view ports to playwright config and use prettier formatting rules.

--- playwright.config.js
+++ playwright.config.js
@@ -1,51 +1,71 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-    testDir: 'tests/playwright',
-    /* Run tests in files in parallel */
-    fullyParallel: true,
-    /* Fail the build on CI if you accidentally left test.only in the source code. */
-    forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
-    /* Run tests in parallel on CI */
-    workers: process.env.CI ? 1 : undefined,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
-    /* Override global timeout. See https://playwright.dev/docs/test-timeouts */
-    timeout: 10000,
+  testDir: "tests/playwright",
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Run tests in parallel on CI */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: "html",
+  /* Override global timeout. See https://playwright.dev/docs/test-timeouts */
+  timeout: 10000,
 
-    use: {
-        // Base URL to use in actions like `await page.goto('/')`
-        // baseURL: 'http://localhost:8080',
-        // Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer
-        trace: 'on-first-retry',
+  use: {
+    // Base URL to use in actions like `await page.goto('/')`
+    // baseURL: 'http://localhost:8080',
+    // Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
-
-    projects: [
-        {
-            name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
-        },
-        // {
-        //     name: "firefox",
-        //     use: { ...devices["Desktop Firefox"] },
-        // },
-
-        // {
-        //     name: "webkit",
-        //     use: { ...devices["Desktop Safari"] },
-        // },
-    ],
-
-    /* Run your local dev server before starting the tests */
-    // webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://127.0.0.1:3000',
-    //   reuseExistingServer: !process.env.CI,
+    // {
+    //     name: "firefox",
+    //     use: { ...devices["Desktop Firefox"] },
     // },
 
-    // Add globalSetup and globalTeardown
-    //globalSetup: require.resolve('./global-setup'),
-    //globalTeardown: require.resolve('./global-teardown'),
+    // {
+    //     name: "webkit",
+    //     use: { ...devices["Desktop Safari"] },
+    // },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+
+  // Add globalSetup and globalTeardown
+  //globalSetup: require.resolve('./global-setup'),
+  //globalTeardown: require.resolve('./global-teardown'),
 });
